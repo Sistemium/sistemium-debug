0b67da629e774e6f3c2ae25dd578e6eec609e529
Alexander Levin <alevin.ru@gmail.com>
2023-10-26 09:51:11 +0300

    ts

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,18 @@ import debugLib from 'debug';
 const { DEBUG_NAMESPACE = 'stm' } = process.env;
 const { ERROR_NAMESPACE = 'stm:error' } = process.env;
 
-export default function (ns) {
+export default function (ns: string) {
   return nsLog(`${DEBUG_NAMESPACE}:${ns}`, `${ERROR_NAMESPACE}:${ns}`);
 }
 
-export function nsLog(ns, nsError = `${ns}:error`) {
+export function nsLog(ns: string, nsError: string = `${ns}:error`) {
   return {
     debug: debug(ns),
     error: error(nsError),
   };
 }
 
-export function debug(ns) {
+export function debug(ns: string) {
 
   const log = debugLib(ns);
   // eslint-disable-next-line
@@ -23,7 +23,7 @@ export function debug(ns) {
 
 }
 
-export function error(ns) {
+export function error(ns: string) {
   const log = debugLib(ns);
   log.log = console.error.bind(console);
   return log;
